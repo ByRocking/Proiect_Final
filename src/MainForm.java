e8d8a10cb4d895039201930378f9ac2acfb63f75
ruspaul12 <ruspaul68@gmail.com>
2021-10-30 18:39:36 +0300

    Inceput implementare logging

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.FileWriter;
+
 
 public class MainForm {
     private JPanel rootPanel;
+
+
+
 
     public static void main(String[] args) {
         JFrame frame=new JFrame("Inventar magazin");
@@ -10,6 +15,15 @@ public class MainForm {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        try{
+            FileWriter fw= new FileWriter("Logging.log");
+            fw.write("test");
+            fw.close();
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        System.out.println("Success...");
 
     }
 
