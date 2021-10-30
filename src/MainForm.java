8575a2160d273706402b0502c81e65270a9d153e
ruspaul12 <ruspaul68@gmail.com>
2021-10-30 17:26:38 +0300

    Creat MainForm

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,4 +1,16 @@
-public class MainForm {
+import javax.swing.*;
+import java.awt.*;
 
+public class MainForm {
+    private JPanel rootPanel;
+
+    public static void main(String[] args) {
+        JFrame frame=new JFrame("Inventar magazin");
+        frame.setContentPane(new MainForm().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
 
 }
