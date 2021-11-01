93210e19f36839545fe71b4f59ac5ca0b054f6a5
ValiVinka <vinkavali@gmail.com>
2021-11-01 18:19:08 +0200

    removed hatz

--- src/GameStop/IJoc.java
+++ src/GameStop/IJoc.java
@@ -1,7 +1,7 @@
 package GameStop;
 
 public interface IJoc {                               // interfata pentru clasa Joc
-    void setNume(String _nume); //hatz
+    void setNume(String _nume);
     String getNume();
     void setPublisher(String _publisher);
     String getPublisher();
